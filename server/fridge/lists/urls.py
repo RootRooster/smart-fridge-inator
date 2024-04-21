cde646a0bdd40335d8fd80bc54a0dfb6baf8137a
Nik Cadez <nikcadez@Niks-MacBook-Pro-2.local>
2024-04-21 07:13:21 +0200

    test11

--- server/fridge/lists/urls.py
+++ server/fridge/lists/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MealPlanViewSet, IngredientsListViewSet, AddIngredientsToList
+from .views import MealPlanViewSet, IngredientsListViewSet, AddIngredientsToList, MealFromIngredientsViewSet
 
 
 routers = routers.DefaultRouter()
 routers.register(r'mealplans', MealPlanViewSet)
 routers.register(r'ingredientslists', IngredientsListViewSet)
-routers.register(r'addtolist', AddIngredientsToList, basename='IngredientsLis')
-
+routers.register(r'addtolist', AddIngredientsToList, basename='IngredientsList')
+routers.register(r'mealfromingredients', MealFromIngredientsViewSet, basename='MealFromIngredients')
 
 urlpatterns = [
     path('', include(routers.urls)),
